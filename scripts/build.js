962a553b2bcab48aa45c6c65e549fb28951c6bbd
Jonathan Doherty <dohjon@users.noreply.github.com>
2025-03-21 10:31:26 +0100

    refactor: tidy

--- scripts/build.js
+++ scripts/build.js
@@ -1,26 +1,22 @@
 import { register } from '@tokens-studio/sd-transforms';
 import StyleDictionary from 'style-dictionary';
+import { getTransforms } from '@tokens-studio/sd-transforms';
 
 register(StyleDictionary, {
   excludeParentKeys: true,
 });
 
 const sd = new StyleDictionary({
-  source: ['tokens/**/*.json'], // 👈 Where your design tokens live
-  preprocessors: ['tokens-studio'], // 👈 This enables Tokens Studio specific processing
+  source: ['tokens/**/*.json'],
+  preprocessors: ['tokens-studio'],
   platforms: {
-    js: {
-      transformGroup: 'tokens-studio', // 👈 apply the tokens-studio transformGroup to apply all transforms
+    ts: {
+      transformGroup: 'tokens-studio',
       buildPath: 'dist/',
-      transforms: [
-        // 'size/pxToRem',
-        // 'name/constant',
-        // 'attribute/cti',
-      ],
       files: [
         {
-          destination: 'tokens.js',
           format: 'javascript/es6',
+          destination: 'tokens.js',
         },
         {
           format: 'typescript/es6-declarations',
