2e9c601c8c048a87c1d236e2f3862cebe992a617
Jonathan Doherty <dohjon@users.noreply.github.com>
2025-03-21 01:24:42 +0100

    chore: tokens

---
+++ scripts/build.js
@@ -0,0 +1,33 @@
+import { register } from '@tokens-studio/sd-transforms';
+import StyleDictionary from 'style-dictionary';
+
+register(StyleDictionary, {
+  excludeParentKeys: true,
+});
+
+const sd = new StyleDictionary({
+  source: ['tokens/**/*.json'], // 👈 Where your design tokens live
+  preprocessors: ['tokens-studio'], // 👈 This enables Tokens Studio specific processing
+  platforms: {
+    js: {
+      transformGroup: 'tokens-studio', // 👈 apply the tokens-studio transformGroup to apply all transforms
+      buildPath: 'build/js/',
+      transforms: [
+        // 'size/pxToRem',
+        // 'name/constant',
+        // 'attribute/cti',
+      ],
+      files: [
+        {
+          destination: 'tokens.js',
+          format: 'javascript/es6',
+        },
+      ],
+    },
+  },
+});
+
+await sd.cleanAllPlatforms();
+await sd.buildAllPlatforms();
+
+console.log('✅ Tokens built!');
